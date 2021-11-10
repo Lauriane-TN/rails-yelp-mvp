5b42db2a27769cf1fd085b2b88dd47fadab3cce8
Lauriane NATHOU <laurianenathou@gmail.com>
2021-11-10 15:18:39 +0100

    App MVC complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,13 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+Restaurant.create!(name: 'Racines', address: "Meriadeck" ,phone_number: 0556,category: "french")
+Restaurant.create!(name: 'Fuxia', address: "Quinconces" ,phone_number: 0556,category: "italian")
+Restaurant.create!(name: 'Le café du Lac', address: "Villenave d'Ornon" ,phone_number: 0556,category: "chinese")
+Restaurant.create!(name: 'NS', address: "Talence thouars" ,phone_number: 0556,category: "japanese")
+Restaurant.create!(name: 'Bistrot Régent', address: "Quai de Baccalan" ,phone_number: 0556,category: "french")
+puts "Finished!"
